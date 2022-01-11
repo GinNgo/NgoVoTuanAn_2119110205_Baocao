f5fe5e7ef0832199f9cbf5cda7400472da924c13
GinNgo <ngovotuananh@gmail.com>
2022-01-11 10:17:13 +0700

    Create file DBConnection

---
+++ Cau1/DAL/DBConnection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cau1.DAL
+{
+    public class DBConnection
+    {
+        public DBConnection()
+        {
+
+        }
+        public SqlConnection CreateConnection()
+        {
+            SqlConnection conn = new SqlConnection
+            {
+                ConnectionString = @"Data Source=DELL-PC\SQLEXPRESS;Initial Catalog=HR;User Id=sa;Password=sa"
+            };
+            return conn;
+        }
+    }
+}
