4bdf4c5b30c456e8d3f5d0bfc870abd3331cd139
GinNgo <ngovotuananh@gmail.com>
2022-01-11 11:30:56 +0700

    create file EmployeeBAL and DepartmentDAl

---
+++ Cau1/BAL/DepartmentBAL.cs
@@ -0,0 +1,20 @@
+﻿using Cau1.BEL;
+using Cau1.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cau1.BAL
+{
+    public class DepartmentBAL
+    {
+        DepartmentDAL dep = new DepartmentDAL();
+        public List<DepartmentBEL> ReadDepartmentList()
+        {
+            List<DepartmentBEL> lstdep = dep.ReadDepartmentList();
+            return lstdep;
+        }
+    }
+}
---
+++ Cau1/BAL/EmloyeeBAL.cs
@@ -0,0 +1,32 @@
+﻿using Cau1.BEL;
+using Cau1.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cau1.BAL
+{
+    public class EmployeeBAL
+    {
+        EmployeeDAL employee = new EmployeeDAL();
+        public List<EmployeeBEL> ReadEmployee()
+        {
+            List<EmployeeBEL> lstCus = employee.ReadEmployee();
+            return lstCus;
+        }
+        public void NewEmployee(EmployeeBEL emp)
+        {
+            employee.NewEmployee(emp);
+        }
+        public void DeleteEmployee(EmployeeBEL emp)
+        {
+            employee.DeleteEmployee(emp);
+        }
+        public void EditEmployee(EmployeeBEL emp)
+        {
+            employee.EditEmployee(emp);
+        }
+    }
+}
