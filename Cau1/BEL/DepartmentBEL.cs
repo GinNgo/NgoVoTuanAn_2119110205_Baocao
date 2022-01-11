7a96025a323dbc0c0ca5fb28316fd04c9694c707
GinNgo <ngovotuananh@gmail.com>
2022-01-11 11:10:47 +0700

    Create EmployeeDAL, DepartmentDAL and change file in folder BEL

--- Cau1/BEL/DepartmentBEL.cs
+++ Cau1/BEL/DepartmentBEL.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Cau1.DAL
+namespace Cau1.BEL
 {
     public class DepartmentBEL
     {
---
+++ Cau1/DAL/DepartmentDAL.cs
@@ -0,0 +1,61 @@
+﻿using Cau1.BEL;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cau1.DAL
+{
+    public class DepartmentDAL : DBConnection
+    {
+
+        public List<DepartmentBEL> ReadDepartmentList()
+        {
+            SqlConnection conn = CreateConnection();
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("SelectAllDepartment", conn)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+            SqlDataReader reader = cmd.ExecuteReader();
+            List<DepartmentBEL> lstArea = new List<DepartmentBEL>();
+            while (reader.Read())
+            {
+                DepartmentBEL area = new DepartmentBEL
+                {
+                    IdDepartment = reader["IdDepartment"].ToString(),
+                    Name = reader["Name"].ToString()
+                };
+                lstArea.Add(area);
+            }
+            conn.Close();
+            return lstArea;
+        }
+
+
+
+        public DepartmentBEL ReadDepartment(string IdDepartment)
+        {
+            SqlConnection conn = CreateConnection();
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("SelectDepartment", conn)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+            cmd.Parameters.Add("@IdDepartment", SqlDbType.NVarChar).Value = IdDepartment;
+            cmd.ExecuteNonQuery();
+            SqlDataReader reader = cmd.ExecuteReader();
+            DepartmentBEL dep = new DepartmentBEL();
+            if (reader.HasRows && reader.Read())
+            {
+                dep.IdDepartment = reader["IdDepartment"].ToString();
+                dep.Name = reader["Name"].ToString();
+            }
+            conn.Close();
+            return dep;
+        }
+    }
+}
